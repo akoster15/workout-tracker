c8a79497de4a05bd8bc3794029e03b6a6bf39687
Andrew Koster <andrewkoster95@gmail.com>
2021-08-14 01:07:42 -0400

    deploy

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-  
-const router = require("express").Router();
-const apiRoutes = require("./apiRoutes");
-const homeRoutes = require("./htmlRoutes");
-
-router.use("/", htmlRoutes);
-router.use("/", apiRoutes);
-
-module.exports = router;
