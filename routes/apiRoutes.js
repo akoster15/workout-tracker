f363fc63e28292915376e611f65d9e8da9e0c5b9
Andrew Koster <andrewkoster95@gmail.com>
2021-08-13 23:39:12 -0400

    added methods to get, post, and push to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,31 @@
-const db = require("../models/Workout");
+const db = require("../models/");
 const router = require("express").Router();
+const mongoose = require("mongoose");
+
+router.post('/api/workouts/', (req, res) => {
+    console.log(req.body)
+        models.Workout.create(req.body)
+          .then((db) => res.json(db))
+          .catch(err => res.status(400).json(err));
+});
+
+router.put('/api/workouts/id:', (req, res) => {
+    console.log(req.body)
+        models.Workout.create(req.body)
+          .then((db) => res.json(db))
+          .catch(err => res.status(400).json(err));
+});
+
+router.get('/api/workouts/', (req, res) => {
+    models.Workout.aggregate( [{
+            $addFields: {
+                totalDuration: {$sum: '$exercises.duration'}
+            }
+        }])
+
+      .then(db => res.json(db))
+      .catch(err => res.status(400).json(err));
+      
+});
+
+module.exports = router;
