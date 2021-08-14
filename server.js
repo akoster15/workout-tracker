a62b82d0e55ee43e6b3871aa1d48cd81ac5febd3
Andrew Koster <andrewkoster95@gmail.com>
2021-08-14 00:45:58 -0400

    finished server.js

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}!`);
+});
