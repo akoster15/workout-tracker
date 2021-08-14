791f7b9e25f317ded438956905b89e6a0c63dc16
Andrew Koster <andrewkoster95@gmail.com>
2021-08-13 23:12:19 -0400

    added index.js to routes directory

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
