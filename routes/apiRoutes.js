8548190f1a123ee464c4aacd3000f29d82731447
Andrew Koster <andrewkoster95@gmail.com>
2021-08-15 21:22:56 -0400

    recreated the mongo data base with a different name, so updated all the files accordingly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,36 +4,45 @@ const db = require("../models/");
 router.get('/api/workouts', (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => {
-      console.log(dbWorkout);
-      res.json(dbWorkout);
+      console.log(workoutdb);
+      res.json(workoutdb);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post('/api/workouts/', (req, res) => {
-    console.log(req.body)
-        models.Workout.create(req.body)
-          .then((db) => res.json(db))
-          .catch(err => res.status(400).json(err));
-});
+router.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+      .then(workoutdb => {
+        res.json(workoutdb);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
-router.put('/api/workouts/id:', (req, res) => {
-    console.log(req.body)
-        models.Workout.create(req.body)
-          .then((db) => res.json(db))
-          .catch(err => res.status(400).json(err));
+router.put('/api/workouts/:id', (req, res) => {
+  db.Workout.updateOne(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
+    .then(dbWorkout => {
+      res.json(workoutdb);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.get('/api/workouts/range', (req, res) => {
   db.Workout.find({})
     .sort({ _id: -1 })
     .limit(7)
-    .then(dbWorkout => {
-      console.log(dbWorkout);
-      res.json(dbWorkout);
-      console.log(dbWorkout);
+    .then(workoutdb => {
+      console.log(workoutdb);
+      res.json(workoutdb);
+      console.log(workoutdb);
     })
     .catch(err => {
       res.json(err);
