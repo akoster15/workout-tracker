a3735a964051826cc50fb38a36b21da2519ec2c0
Andrew Koster <andrewkoster95@gmail.com>
2021-08-15 20:53:37 -0400

    made some fixes to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,16 @@ const db = require("../models/");
 const router = require("express").Router();
 
 router.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-  
+  db.Workout.find({})
+    .then(dbWorkout => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 router.post('/api/workouts/', (req, res) => {
     console.log(req.body)
         models.Workout.create(req.body)
@@ -27,16 +27,18 @@ router.put('/api/workouts/id:', (req, res) => {
           .catch(err => res.status(400).json(err));
 });
 
-router.get('/api/workouts/', (req, res) => {
-    models.Workout.aggregate( [{
-            $addFields: {
-                totalDuration: {$sum: '$exercises.duration'}
-            }
-        }])
-
-      .then(db => res.json(db))
-      .catch(err => res.status(400).json(err));
-
+router.get('/api/workouts/range', (req, res) => {
+  db.Workout.find({})
+    .sort({ _id: -1 })
+    .limit(7)
+    .then(dbWorkout => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+      console.log(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
