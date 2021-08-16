53befac18537661d6db7a86f16baf847888538e4
Andrew Koster <andrewkoster95@gmail.com>
2021-08-15 20:16:55 -0400

    updated Workout.js to show more specified details in exercises array

--- models/Workout.js
+++ models/Workout.js
@@ -6,7 +6,40 @@ const WorkoutSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  exercises: Array,
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: "Type is Required"
+    },
+    name: {
+      type: String,
+      unique: true,
+      required: true
+    },
+
+    duration: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    weight: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    reps: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    sets: {
+      type: Number,
+      unique: true,
+      required: true
+    }
+  }
+  ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
